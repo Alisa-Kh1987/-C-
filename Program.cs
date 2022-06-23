b7847223fecb64a60a85b0ce068e12da86acad52
Алиса <petrunina.alisa@gmail.com>
2022-06-23 22:24:31 +0300

    .\Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//запрашиваем число
+//Console.Write("Input number:");
+
+//создаем переменную number и сохраняем в нее значение, введенное из консоли
+//int number = Convert.ToInt32 (Console.ReadLine());
+
+//выводим значение переменной number
+//Console.WriteLine(number + "->" + number*number);
+
+//вводим первое число
+//Console.Write("Input first number:");
+//сохраняем значение в переменную
+//int number_1 = Convert.ToInt32 (Console.ReadLine());
+//Console.Write("Input second number:");
+//int number_2 = Convert.ToInt32 (Console.ReadLine());
+//оцениваем, является ли первое число квадратом второго
+//if (number_1 == number_2*number_2)
+   // Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + " yes");
+//else
+    //Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + " no");
+
