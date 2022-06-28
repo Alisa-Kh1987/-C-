aa43b6f358fc170ee3fcec3840346ee2953ca38e
Алиса <petrunina.alisa@gmail.com>
2022-06-28 23:27:54 +0300

    Закомментирована задача 6

--- Program.cs
+++ Program.cs
@@ -77,14 +77,14 @@
 
 //Задача № 6.
 
-Console.Write("Input number:");
-int number = Convert.ToInt32 (Console.ReadLine());
-if (number %2 ==0){
-    Console.WriteLine("число четное");
-}
-else {
-    Console.WriteLine("число нечетное");
-}
+//Console.Write("Input number:");
+//int number = Convert.ToInt32 (Console.ReadLine());
+//if (number %2 ==0){
+//    Console.WriteLine("число четное");
+//}
+//else {
+//    Console.WriteLine("число нечетное");
+//}
 
 
 
