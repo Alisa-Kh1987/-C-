6ad206a259652023d0f7fb556bfe42aa4b4a0733
Алиса <petrunina.alisa@gmail.com>
2022-06-28 22:33:21 +0300

    Решение задачи 4 к семинару 1 по С#

--- Program.cs
+++ Program.cs
@@ -23,13 +23,54 @@
 
 
 //Домашнее задание к семинару № 1 по C#
+
+//Задача № 2.
+
+//вариант разбора на семинаре
+
+//Console.Write("Input first number:");
+//int number_1 = Convert.ToInt32 (Console.ReadLine());
+//Console.Write("Input second number:");
+//int number_2 = Convert.ToInt32 (Console.ReadLine());
+//if (number_1 > number_2){
+//Console.WriteLine(number_1 + " больше " + number_2);
+//}
+//else if (number_1 < number_2){
+ //   Console.WriteLine(number_1 + " меньше " + number_2);
+//}
+//else {
+ //  Console.WriteLine("значения чисел одинаковы");
+//}
+
+//Свой первоначальный вариант (не совсем верно учла условие, ЧТО надо вывести на экран - не столько максимум, сколько то, какое число больше/меньше второго. Ну и про лишнее if не учла)
+
+//Console.Write("Input first number:");
+//int number_1 = Convert.ToInt32 (Console.ReadLine());
+//Console.Write("Input second number:");
+//int number_2 = Convert.ToInt32 (Console.ReadLine());
+//if (number_1 > number_2)
+//Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + " max == a");
+//else 
+//    Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + "max == b");
+//if (number_1 == number_2)
+//   Console.WriteLine("a = " + number_1 + ", b = " + number_2 + "->" + " max is not found. The values of the variables are equal");
+
+//Задача 4
+
 Console.Write("Input first number:");
 int number_1 = Convert.ToInt32 (Console.ReadLine());
 Console.Write("Input second number:");
 int number_2 = Convert.ToInt32 (Console.ReadLine());
-if (number_1 > number_2)
-   Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + " max == a");
-else
-    Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> " + "max == b");
-if (number_1 == number_2)
-   Console.WriteLine("a = " + number_1 + ", b = " + number_2 + "->" + " max is not found. The values of the variables are equal");
+Console.Write("Input third number:");
+int number_3 = Convert.ToInt32 (Console.ReadLine());
+if (number_1>number_2 && number_1>number_3){
+    Console.WriteLine("max: " + number_1);
+    }
+else if (number_2>number_1 && number_2 > number_3){
+    Console.WriteLine("max: " + number_2);
+    }
+else if (number_3 > number_1 && number_3 > number_2){
+    Console.WriteLine("max: " + number_3);
+    }
+else {Console.WriteLine("Введенные значения одинаковы");
+}
